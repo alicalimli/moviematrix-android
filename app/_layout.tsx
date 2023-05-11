7fc4d4ff236e58a766d502b2b3852ee5faf45c9e
alicalimli <alicalimli76@gmail.com>
2023-05-11 20:53:38 +0300

    [FEAT] Used React Query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
 import { Stack } from "expo-router";
 import { store } from "../redux/store";
+import { Text } from "react-native";
 import { Provider } from "react-redux";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const Layout = () => {
   return (
-    <Provider store={store}>
-      <Stack />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Stack />
+      </Provider>
+    </QueryClientProvider>
   );
 };
 
--- components/card-details/CardDetails.tsx
+++ components/card-details/CardDetails.tsx
@@ -8,6 +8,7 @@ import {
   FlatList,
   ScrollView,
   TouchableOpacity,
+  ActivityIndicator,
 } from "react-native";
 import { useAppDispatch, useAppSelector } from "../../hooks/use-redux";
 import {
@@ -16,6 +17,22 @@ import {
   removeBookmark,
 } from "../../redux/slices/bookmarksSlice";
 import { API_KEY } from "../../config";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchDetails = async (mediaType: "movie" | "tv", id: number) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${mediaType}/${id}?api_key=${API_KEY}`
+  );
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    throw new Error(data.status_message);
+  } else {
+    return {
+      details: data,
+    };
+  }
+};
 
 interface CardDetailsProps {}
 
@@ -27,25 +44,16 @@ const CardDetails = ({ id, type }: CardDetailsProps) => {
 
   const [movie, setMovie] = useState(null);
 
-  useEffect(() => {
-    const fetchMovie = async () => {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}`
-      );
-      const data = await response.json();
-      setMovie(data);
-    };
-
-    fetchMovie();
-  }, [id]);
-
-  if (!movie) {
-    return <Text>Loading...</Text>;
-  }
+  const { data, error, isLoading } = useQuery({
+    queryKey: [type, id],
+    queryFn: () => fetchDetails(type, id),
+  });
 
   const onBookmarkPress = (detail: any) => {
     dispatch(isBookmarked ? removeBookmark(+detail.id) : addBookmark(detail));
   };
+
+  const details = data?.details;
 
   return (
     <SafeAreaView className="bg-[#111] flex-1">
@@ -58,40 +66,52 @@ const CardDetails = ({ id, type }: CardDetailsProps) => {
         }}
       />
 
-      <ScrollView>
-        <View className="relative">
-          <Image
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
-            }}
-            resizeMode="cover"
-            className="w-full h-96"
-          />
-        </View>
+      <View>
+        {isLoading ? (
+          <ActivityIndicator size="large" />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <ScrollView>
+            <View className="relative">
+              <Image
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500${details?.poster_path}`,
+                }}
+                resizeMode="cover"
+                className="w-full h-96"
+              />
+            </View>
 
-        <View className="p-2">
-          <View className="flex flex-row flex-wrap items-center">
-            <Text className="text-[#fff] text-2xl font-medium">
-              {movie.title}
-            </Text>
-            <TouchableOpacity
-              className="ml-auto"
-              onPress={() => onBookmarkPress(movie)}
-            >
-              <Text
-                className={`
-                ${isBookmarked ? "bg-[#20C997] text-[#fff]" : "text-[#20C997] "}
-                border border-[#20C997] p-2 px-6 rounded-md text-lg 
-              `}
-              >
-                {isBookmarked ? "Bookmarked" : "Bookmark"}
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <Text className="text-[#fff] text-lg">{movie.overview}</Text>
-          <Text className="text-[#fff] text-lg">{movie.overview}</Text>
-        </View>
-      </ScrollView>
+            <View className="p-2">
+              <View className="flex flex-row flex-wrap items-center">
+                <Text className="text-[#fff] text-2xl font-medium">
+                  {details?.title}
+                </Text>
+                <TouchableOpacity
+                  className="ml-auto"
+                  onPress={() => onBookmarkPress(details)}
+                >
+                  <Text
+                    className={`
+                  ${
+                    isBookmarked
+                      ? "bg-[#20C997] text-[#fff]"
+                      : "text-[#20C997] "
+                  }
+                  border border-[#20C997] p-2 px-6 rounded-md text-lg 
+                `}
+                  >
+                    {isBookmarked ? "Bookmarked" : "Bookmark"}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <Text className="text-[#fff] text-lg">{details?.overview}</Text>
+              <Text className="text-[#fff] text-lg">{details?.overview}</Text>
+            </View>
+          </ScrollView>
+        )}
+      </View>
     </SafeAreaView>
   );
 };
