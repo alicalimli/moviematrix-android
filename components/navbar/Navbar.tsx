8ce67efdc39459b5ab7daa5ef558079add078ccd
alicalimli <alicalimli76@gmail.com>
2023-05-12 10:29:07 +0300

    [FEAT] Highlight Active Link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { Link } from "expo-router";
+import { Link, usePathname } from "expo-router";
 
 const Navbar = () => {
-  // const route = useRoute();
+  const pathname = usePathname();
 
   const links = [
     { name: "Home", routeName: "home" },
@@ -14,18 +14,23 @@ const Navbar = () => {
   ];
 
   return (
-    <View className="flex flex-row justify-between bg-gray-100 p-4">
-      {links.map((link) => (
-        <Link
-          key={link.routeName}
-          href={link.routeName}
-          // className={`px-4 py-2 rounded-lg ${
-          //   route.name === link.routeName ? "bg-gray-200" : ""
-          // }`}
-        >
-          <Text className="text-gray-700 font-medium">{link.name}</Text>
-        </Link>
-      ))}
+    <View className="flex flex-row justify-between bg-black p-4">
+      {links.map((link) => {
+        const isActive = pathname.includes(link.routeName);
+
+        return (
+          <Link key={link.routeName} href={link.routeName}>
+            <Text
+              className={`
+                px-4 py-2 rounded-lg font-medium
+                ${isActive ? "text-[#20C997]" : "text-[#fff]"}
+              `}
+            >
+              {link.name}
+            </Text>
+          </Link>
+        );
+      })}
     </View>
   );
 };
